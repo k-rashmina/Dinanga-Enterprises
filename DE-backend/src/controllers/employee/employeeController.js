034f98901c48372a55570455b8689acdff38f4b4
EGManeeshaDissanayake <maneeshadissanayakeeg@gmail.com>
2024-03-16 17:00:05 +0530

    Added employee controller file

---
+++ DE-backend/src/controllers/employee/employeeController.js
@@ -0,0 +1,23 @@
+// Controller for retrieving employee details
+const getEmployeeTest = async (req, res) => {
+    return res.status(200).json({ msg: 'Test employee' })
+    // try {
+    //   const employee = await Employee.findById(req.params.id);
+    //   if (!employee) {
+    //     return res.status(404).json({ msg: 'Employee not found' });
+    //   }
+    //   res.json(employee);
+    // } catch (err) {
+    //   console.error(err.message);
+    //   res.status(500).send('Server Error');
+    // }
+  };
+
+
+module.exports = {
+    getEmployeeTest,
+    // registerEmployee,
+    // getEmployeeDetails,
+    // deleteEmployee,
+    // updateEmployee,
+  };
