37672ce93fdc0636c2b956b015561428b52ee32a
Bethmi Jithara <it22336590@my.sliit.lk>
2024-03-16 14:23:45 +0530

    Create jobappointment, create jobAdmin,create jobCustomer

---
+++ DE-frontend/src/components/job/jobappointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const jobappointment = () => {
+    return (
+
+        <h1>jobAppointment</h1>
+    )
+}
+
+export default jobappointment;
