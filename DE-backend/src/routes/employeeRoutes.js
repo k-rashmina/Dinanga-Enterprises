b4b4ef5b59ed7abc0a6d229440f880f00dd421a1
EGManeeshaDissanayake <maneeshadissanayakeeg@gmail.com>
2024-03-17 14:44:43 +0530

    Added CRUD routes

--- DE-backend/src/routes/employeeRoutes.js
+++ DE-backend/src/routes/employeeRoutes.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const {getEmployeeTest} = require('../controllers/employee/employeeController');
+const {getEmployeeTest, registerEmployee, getEmployeeDetails, deleteEmployee, updateEmployee, getAssignedTasks, getCompletedTasks, getAssignedServices} = require('../controllers/employee/employeeController');
 
 const router = express.Router();
 
+// test route
 router.get('/test', getEmployeeTest);
+
+router.post('/registerEmployee', registerEmployee);
+
+router.get('/getEmployeeDetails/:id',getEmployeeDetails);
+
+router.delete('/deleteEmployee/:id', deleteEmployee);
+
+router.put('/updateEmployee/:id', updateEmployee);
+
+router.get('/getAassignedTasks', getAssignedTasks);
+
+router.get('/getCompletedTasks', getCompletedTasks);
+
+router.get('/getAssignedServices', getAssignedServices);
+
+
 
 module.exports = router;
