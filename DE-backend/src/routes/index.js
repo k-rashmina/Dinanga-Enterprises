967cc1b6c83a7b2a5a07c22f05bfb5f94b7bb28d
EGManeeshaDissanayake <maneeshadissanayakeeg@gmail.com>
2024-03-17 15:22:11 +0530

    Added employee routes to index

--- DE-backend/src/routes/index.js
+++ DE-backend/src/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const employeeRoutes = require("./employeeRoutes");
 
 const router = express.Router();
 
+//Employee Routes
+router.use('/employee',employeeRoutes);
 
 module.exports = router;
