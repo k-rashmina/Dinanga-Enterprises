1b76b8a96303f054d71d6e348de57b988582bc75
asirialwis <asirichathurika900@gmail.com>
2024-03-29 08:22:58 +0530

    form init and validate

--- DE-frontend/src/components/inventory/AddItem.jsx
+++ DE-frontend/src/components/inventory/AddItem.jsx
@@ -1,9 +1,282 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import "./AddItems.css";
 
 function AddItem() {
+  const [itemNumber, setItemNumber] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [reorderLevel, setReorderLevel] = useState("");
+  const [itemPrice, setItemPrice] = useState("");
+  const [availability, setAvailability] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateItemName = () => {
+    const errorsCopy = { ...errors };
+    if (!itemName) errorsCopy.itemName = "Item name is required";
+    else delete errorsCopy.itemName;
+    setErrors(errorsCopy);
+  };
+
+  const validateItemNumber = () => {
+    const errorsCopy = { ...errors };
+    if (!itemNumber) errorsCopy.itemNumber = "Item number is required";
+    else if(!/^DE\d{3}$/i.test(itemNumber)) errorsCopy.itemNumber = "Item number must start with DE followed by 3 digits";
+    else delete errorsCopy.itemNumber;
+    setErrors(errorsCopy);
+  };
+
+  const validateQuantity = () => {
+    const errorsCopy = { ...errors };
+    if (!quantity) errorsCopy.quantity = "Quantity is required";
+    else if (!/^\d+$/.test(quantity))
+      errorsCopy.quantity = "Quantity must be a number";
+    else delete errorsCopy.quantity;
+    setErrors(errorsCopy);
+  };
+
+  const validateReorderLevel = () => {
+    const errorsCopy = { ...errors };
+    if (!reorderLevel) errorsCopy.reorderLevel = "Reorder level is required";
+    else if (!/^\d+$/.test(reorderLevel))
+      errorsCopy.reorderLevel = "Reorder level must be a number";
+    else delete errorsCopy.reorderLevel;
+    setErrors(errorsCopy);
+  };
+
+  const validateItemPrice = () => {
+    const errorsCopy = { ...errors };
+    if (!itemPrice) errorsCopy.itemPrice = "Item price is required";
+    else if (!/^\d+$/.test(itemPrice))
+      errorsCopy.itemPrice = "Item Price must be a number";
+    else delete errorsCopy.itemPrice;
+    setErrors(errorsCopy);
+  };
+
+  const validateAvailability = () => {
+    const errorsCopy = { ...errors };
+    if (!availability) errorsCopy.availability = "Availability is required";
+    else delete errorsCopy.availability;
+    setErrors(errorsCopy);
+  };
+
+  function sendData(e) {
+    e.preventDefault();
+    validateItemName() &
+      validateItemNumber() &
+      validateQuantity() &
+      validateReorderLevel() &
+      validateItemPrice() &
+      validateAvailability();
+    // Check if there are any errors after validation
+    const noErrors = Object.keys(errors).length === 0;
+    if (noErrors) {
+      const newItem = {
+        itemNumber,
+        itemName,
+        quantity,
+        reorderLevel,
+        itemPrice,
+        availability,
+      };
+      axios
+        .post("http://localhost:5000/inventory/add", newItem)
+        .then(() => {
+          alert("Item Added");
+        })
+        .catch((err) => {
+          alert("Please fill all the fields");
+        });
+    }
+  }
+
   return (
-    <div>AddItem</div>
-  )
+    <div>
+      <div class="container-fluid px-1 py-5 mx-auto">
+        <div class="row d-flex justify-content-center">
+          <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+            <h3>List a New Item</h3>
+            <p class="blue-text">
+              Just answer a few questions
+              <br /> so that we can personalize the right experience for you.
+            </p>
+            <div class="card">
+              <h5 class="text-center mb-4">
+                Add Details for warehouse records
+              </h5>
+              <form className="form-card" onSubmit={sendData}>
+                <div class="row justify-content-between text-left">
+                  <div class="row justify-content-between text-left">
+                    <div class="form-group col-12 flex-column d-flex mb-3">
+                      <label class="form-control-label px-3">
+                        New Item Name
+                        <span class="text-danger"> *</span>
+                      </label>
+
+                      <input
+                        type="text"
+                        id="ans"
+                        name="ans"
+                        // class="form-control"
+                        className={`form-control ${
+                          errors.itemName ? "is-invalid" : ""
+                        }`}
+                        placeholder="Item Name"
+                        value={itemName}
+                        onChange={(e) => setItemName(e.target.value)}
+                        onBlur={validateItemName}
+                      />
+                      {errors.itemName && (
+                        <div className="invalid-feedback">
+                          {errors.itemName}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  <div class="row justify-content-between text-left">
+                    <div class="form-group col-md-6 mb-3">
+                      <label class="form-control-label px-3">
+                        Item Number<span class="text-danger"> *</span>
+                      </label>
+                      <input
+                        type="text"
+                        id="fname"
+                        name="fname"
+                        // class="form-control"
+                        className={`form-control ${
+                          errors.itemNumber ? "is-invalid" : ""
+                        }`}
+                        placeholder="Enter item number"
+                        value={itemNumber}
+                        onChange={(e) => setItemNumber(e.target.value)}
+                        onBlur={validateItemNumber}
+                      />
+                      {errors.itemNumber && (
+                        <div className="invalid-feedback">
+                          {errors.itemNumber}
+                        </div>
+                      )}
+                    </div>
+
+                    <div class="form-group col-md-6 mb-3">
+                      <label class="form-control-label px-3">
+                        Quantity<span class="text-danger"> *</span>
+                      </label>
+                      <input
+                        type="text"
+                        id="lname"
+                        name="lname"
+                        // class="form-control"
+                        className={`form-control ${
+                          errors.quantity ? "is-invalid" : ""
+                        }`}
+                        placeholder="Enter quantity"
+                        value={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                        onBlur={validateQuantity}
+                      />
+                      {errors.quantity && (
+                        <div className="invalid-feedback">
+                          {errors.quantity}
+                        </div>
+                      )}
+                    </div>
+
+                    <div class="form-group col-md-6 mb-3">
+                      <label class="form-control-label px-3">
+                        Re Order Level<span class="text-danger"> *</span>
+                      </label>
+                      <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        // class="form-control"
+                        className={`form-control ${
+                          errors.reorderLevel ? "is-invalid" : ""
+                        }`}
+                        placeholder=""
+                        value={reorderLevel}
+                        onChange={(e) => setReorderLevel(e.target.value)}
+                        onBlur={validateReorderLevel}
+                      />
+                      {errors.reorderLevel && (
+                        <div className="invalid-feedback">
+                          {errors.reorderLevel}
+                        </div>
+                      )}
+                    </div>
+                    <div class="form-group col-md-6 mb-3">
+                      <label class="form-control-label px-3">
+                        Unit Price<span class="text-danger"> *</span>
+                      </label>
+                      <input
+                        type="text"
+                        id="mob"
+                        name="mob"
+                        // class="form-control"
+                        className={`form-control ${
+                          errors.itemPrice ? "is-invalid" : ""
+                        }`}
+                        placeholder=""
+                        value={itemPrice}
+                        onChange={(e) => setItemPrice(e.target.value)}
+                        onBlur={validateItemPrice}
+                      />
+                      {errors.itemPrice && (
+                        <div className="invalid-feedback">
+                          {errors.itemPrice}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  <div class="row justify-content-between text-left">
+                    <div class=" form-group col-12 flex-column d-flex mb-3">
+                      <label class="form-control-label px-3 ">
+                        Availability<span class="text-danger"> *</span>
+                      </label>
+                      <select
+                        // class="form-select"
+                        className={`form-control ${
+                          errors.availability ? "is-invalid" : ""
+                        }`}
+                        id="availability"
+                        value={availability}
+                        onChange={(e) => setAvailability(e.target.value)}
+                        onBlur={validateAvailability}
+                      >
+                        <option value="">Select availability...</option>
+                        <option value="Available">Available</option>
+                        <option value="Reorder">Reorder</option>
+                      </select>
+                      {errors.availability && (
+                        <div className="invalid-feedback">
+                          {errors.availability}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  <div class="row justify-content-end">
+                    <div class="form-group col-sm-6">
+                      <button
+                        type="submit"
+                        class="btn-block btn-primary"
+                        style={{ backgroundColor: "#00BCD4" }}
+                      >
+                        List Item
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddItem
+export default AddItem;
