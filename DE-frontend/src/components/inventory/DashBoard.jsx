41d9bbf849cbf7ef75529e3a89a62015426bb285
asirialwis <asirichathurika900@gmail.com>
2024-03-30 21:37:46 +0530

    stock status chart component init

--- DE-frontend/src/components/inventory/DashBoard.jsx
+++ DE-frontend/src/components/inventory/DashBoard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CardList from "./cards/CardList";
 import InventoryStockValueChart from "./charts/InventoryStockValueChart";
+import InventoryStockStatus from "./charts/InventoryStockStatus";
 function DashBoard() {
   return (
     <div>
       <CardList/>
       <InventoryStockValueChart/>
+      <InventoryStockStatus/>
       <div className="card mb-3" style={{width: "18rem"}}>
         <div className="card-body">
           <h5 className="card-title">Special title treatment</h5>
