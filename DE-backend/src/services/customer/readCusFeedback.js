ab87656bbedcd124da108de5a1510892cd9f16d1
Amasha Hewagama <hewagamadilhani6@gmail.com>
2024-04-22 12:34:38 +0530

    cus changes

--- DE-backend/src/services/customer/readCusFeedback.js
+++ DE-backend/src/services/customer/readCusFeedback.js
@@ -1,8 +1,8 @@
 const {getFeedbackCustomer} = require('../../data-access/customer/customerFeedbackDB');
 
-module.exports = async function customerFeedback() {
+module.exports = async function customerFeedback(cusemail) {
 
-    const Feedbacks = await getFeedbackCustomer();
+    const Feedbacks = await getFeedbackCustomer(cusemail);
 
     return(Feedbacks);
 
