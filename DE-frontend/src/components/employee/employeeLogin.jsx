5a4da59d7bd9df38ead08d7b7a288b801c5d9d11
EGManeeshaDissanayake <maneeshadissanayakeeg@gmail.com>
2024-04-23 19:42:05 +0530

    Fixed login issue

--- DE-frontend/src/components/employee/employeeLogin.jsx
+++ DE-frontend/src/components/employee/employeeLogin.jsx
@@ -2,28 +2,44 @@ import React, { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import EmployeeApiService from "./EmployeeServices";
+import {jwtDecode} from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 const EmployeeLogin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await EmployeeApiService.loginEmployee(
-        username,
-        password
-      );
+        const response = await EmployeeApiService.loginEmployee(username, password);
+        console.log("Login successful:", response);
 
-      console.log("Login successful:", response);
+        if (response.token) {
+            const decodedToken = jwtDecode(response.token);
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('emp_id', decodedToken.employeeId);
+            
 
-      setUsername("");
-      setPassword("");
+            if (localStorage.getItem('token')) {
+              navigate('/empprofile');
+            } else {
+                alert('Failed to store token. Please try logging in again.');
+            }
+        } else {
+            alert('Login failed: ' + response.message);
+        }
+
+        setUsername("");
+        setPassword("");
     } catch (error) {
-      console.error("Login failed:", error);
+        console.error("Login failed:", error);
+        alert('Incorrect username or password.');
     }
-  };
+};
+
 
   return (
     <Container style={{ marginTop: "50px" }}>
