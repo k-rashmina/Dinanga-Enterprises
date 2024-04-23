d8744eb57a86e7b9bfa0cd9e2b155274c5d8a4ad
chamath <chamaththilakarathna10@gmail.com>
2024-04-23 13:13:20 +0530

    cus report button replace

--- DE-frontend/src/components/common/AdminPanel.jsx
+++ DE-frontend/src/components/common/AdminPanel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ReportButton from '../customer/customerReport';
 
 export default function AdminPanel() {
 
@@ -12,6 +13,7 @@ export default function AdminPanel() {
         <div className="me-5"><Link to='order/dashboard'>Orders</Link></div>
         <div className="me-5"><Link to='consultancy/consultancylist'>Consultancy</Link></div>
         <div className="me-5"><Link to='employee/dashboard'>Employee</Link></div>
+        <div><ReportButton/></div>
     </>
 
   )
--- DE-frontend/src/components/customer/ProfilePicture.jsx
+++ DE-frontend/src/components/customer/ProfilePicture.jsx
@@ -13,7 +13,7 @@ const ProfilePicture = ({ imageUrl }) => {
   return (
     <div className="text-center">
       <img src={imageUrl} alt="Profile Picture" className="rounded-circle" style={{ width: '150px', height: '150px' }} />
-      <h4>'{userDetails.cusFname}'</h4>
+      <h4>{userDetails.cusFname}</h4>
       <h4>{userDetails.cusMail}</h4>
     </div>
   );
