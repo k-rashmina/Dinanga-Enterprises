82ad8aa2174b4e7b16d661189217a763c5729057
asirialwis <asirichathurika900@gmail.com>
2024-05-14 08:11:16 +0530

    bug fixed

--- DE-frontend/src/components/inventory/AddItem.jsx
+++ DE-frontend/src/components/inventory/AddItem.jsx
@@ -104,7 +104,7 @@ function AddItem() {
     <div class="con">
       <AdminHeader pageName={"Add Item"} />
       <SearchBar />
-      <div class="container-fluid px-1 py-5 mx-auto">
+      <div class="container-fluid px-1 py-5 mx-auto" style={{ marginTop: '-60px' }}>
         <div class="row d-flex justify-content-center">
           <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
             <div
